b37c02828874ea1c92af08dabc49a3f9924cc57c
DewniNimaya <dew@9712>
2021-04-16 09:39:59 +0530

    initial commit

---
+++ InnovativeProjectService/src/util/DBConnection.java
@@ -0,0 +1,29 @@
+package util;
+
+import java.sql.*;
+
+
+public class DBConnection {
+	
+	public static Connection connect()
+	{ 
+	 Connection con = null; 
+	 
+	 try 
+	 { 
+	 Class.forName("com.mysql.jdbc.Driver"); 
+	 con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", 
+	 "root", ""); 
+	 //For testing
+	 System.out.print("Successfully connected"); 
+	 } 
+	 catch(Exception e) 
+	 { 
+	 e.printStackTrace(); 
+	 } 
+	 
+	 return con; 
+	}
+
+
+}
